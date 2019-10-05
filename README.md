6d9de650f118ecd2a962f9a9735e0dce37ac8623
Jury Verrigni <jury.verrigni@gmail.com>
2019-10-05 20:22:59 +0100

    Add build-switch.sh

---
+++ README.md
@@ -0,0 +1,8 @@
+libicon-switch
+==============
+
+Based on libiconv-1.16 this is just a wrapper around devkitpro toolchain to easily build libiconv and add it to the prefix.
+./build-switch.sh
+
+
+Comes with autotools auto-generated clutter for free.
---
+++ build-switch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CPUS=$((`nproc --all` + 1))
+source /opt/devkitpro/switchvars.sh
+./configure --host=aarch64-none-elf --prefix=/opt/devkitpro/portlibs/switch/ --exec-prefix=/opt/devkitpro/portlibs/switch/
+cd libcharset
+make install -j`echo $CPUS`
+cd ../lib
+make install -j`echo $CPUS`
